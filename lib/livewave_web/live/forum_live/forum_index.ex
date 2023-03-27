f76ff26122645d26d5495c0563c85b2af1c1abed
Thomas <tfnielson.se@gmail.com>
2023-03-27 16:56:26 -0600

    implemented create a new forum feature

--- lib/livewave_web/live/forum_live/forum_index.ex
+++ lib/livewave_web/live/forum_live/forum_index.ex
@@ -4,7 +4,7 @@ defmodule LivewaveWeb.ForumLive.ForumIndex do
   alias Livewave.Repo
 
   alias Livewave.Accounts
-  alias Livewave.Accounts.User
+  # alias Livewave.Accounts.User
 
   alias Livewave.Posts
   alias Livewave.Posts.Comments
@@ -13,23 +13,31 @@ defmodule LivewaveWeb.ForumLive.ForumIndex do
   alias Livewave.Rooms.Forum
 
   def mount(_params, %{"user_id" => user_id}, socket) do
-
-    {:ok, assign(socket,
-      current_user: Accounts.get_user!(user_id),
-      forums: Rooms.list_forums,
-    )}
+    {:ok,
+     assign(socket,
+       current_user: Accounts.get_user!(user_id),
+       forums: Rooms.list_forums()
+     )}
   end
 
-  def handle_event("new-forum", _params, socket) do
-    # Rooms.create_forum(Forum, )
+  def handle_event("new-forum", %{"forum_name" => forum_name}, socket) do
+
+    IO.inspect(Rooms.create_forum(%{name: forum_name}))
+
     {:noreply, socket}
   end
 
   def render(assigns) do
     ~H"""
     <div>
-      <h1> Forums </h1>
-      <button phx-click="new-forum"> Create New Forum Post </button>
+      <h1>Forums</h1>
+      <form phx-submit="new-forum">
+        <input type="text" name="forum_name" placeholder="type your forum question here..." />
+        <button>Create New Forum Post</button>
+      </form>
+      <%= for forum <- @forums do %>
+      <%= forum.name %>
+      <% end %>
     </div>
     """
   end
